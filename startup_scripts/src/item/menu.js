218b6035c0504567289223a59173ef636b8590dd
D95700 <1828339558@qq.com>
2026-06-09 16:36:06 +0800

    重构文件管理结构

--- startup_scripts/src/item/menu.js
+++ startup_scripts/src/item/menu.js
@@ -6,12 +6,3 @@ StartupEvents.registry('item', event => {
         .glow(true)
         .maxStackSize(1)
 })
-
-// 设置稀有度、显示名称、隐藏 tooltip
-ItemEvents.modification(event => {
-    event.modify('kubejs:menu', item => {
-        item.set('minecraft:rarity', 'epic')
-        item.setItemName(Component.string('§6菜 单'))
-        item.set('minecraft:hide_tooltip', true)
-    })
-})
