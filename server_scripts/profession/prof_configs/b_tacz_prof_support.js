3f981f727f507a97195a99b5cf4865e8c9ccb7b7
xkmxz <aw53978296@163.com>
2026-06-11 16:37:51 +0800

    新增支持的枪械配置和配件信息，更新枪械弹药类型描述

--- server_scripts/profession/prof_configs/b_tacz_prof_support.js
+++ server_scripts/profession/prof_configs/b_tacz_prof_support.js
@@ -38,6 +38,17 @@ PROF_CONFIGS.support = {
           extended_mag: [{id:'tacz:extended_mag_1'},{id:'tacz:extended_mag_2'},{id:'tacz:extended_mag_3'}],
         },
       },
+      bf1liu: {
+        gunId: 'bf1:liu', GunFireMode: 'AUTO', GunCurrentAmmoCount: 50,
+        HasBulletInBarrel: $ByteTag.valueOf(1),
+        ammo: { ammoId: 'tacz:556x45', main: 350, level: 3 },
+        attachments: {
+          scope: [{id:'tacz:sight_t1'},{id:'tacz:sight_t2'},{id:'tacz:sight_552'},{id:'tacz:scope_reflex'}],
+          muzzle: [{id:'tacz:muzzle_brake_cthulhu'},{id:'tacz:muzzle_brake_cyclone_d2'},{id:'tacz:muzzle_brake_pioneer'},{id:'tacz:muzzle_compensator_trident'}],
+          extended_mag: [{id:'tacz:extended_mag_1'},{id:'tacz:extended_mag_2'},{id:'tacz:extended_mag_3'}],
+          grip: [{id:'tacz:grip_vertical_military'},{id:'tacz:grip_rk0'},{id:'tacz:grip_cqr'}],
+        },
+      }
     },
     secondary: {
       deagle: {
