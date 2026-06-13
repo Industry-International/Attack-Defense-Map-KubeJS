ef19dcde39be204f1f57bcef0edc6f52f3225c32
IdealistSl <2478189960@qq.com>
2026-06-13 19:16:31 +0800

    弹甲相关参数迁移

---
+++ startup_scripts/taov_returned_autocannon.js
@@ -0,0 +1,11 @@
+// TAOV Returned aircraft weapon runtime settings.
+// muzzleVelocityBlocksPerSecond uses Minecraft blocks per second. 200 = 10 blocks/tick.
+global.taovReturnedAutocannon = {
+  muzzleVelocityBlocksPerSecond: 200,
+  forceTracer: false,
+  propellerSoundEnabled: false,
+  rocketRackReloadSeconds: 20,
+  kamikazeExplosivePower: 12
+}
+
+console.info('[TAOV Returned] Aircraft settings: muzzleVelocityBlocksPerSecond=200, forceTracer=false, propellerSoundEnabled=false, rocketRackReloadSeconds=20, kamikazeExplosivePower=12')
