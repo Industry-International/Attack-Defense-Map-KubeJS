cba1ce53cc247b3a11389be4eadcf560eebe984a
xkmxz <aw53978296@163.com>
2026-07-06 18:38:44 +0800

    feat: 更新自杀笔记事件处理，修改使用消息为“你已重新部署！”并优化代码逻辑

--- server_scripts/suicide_note.js
+++ server_scripts/suicide_note.js
@@ -7,9 +7,9 @@ ItemEvents.rightClicked('kubejs:suicide_note', function(event) {
   var player = event.player
   if (event.hand !== 'main_hand') return
 
+  // 执行重新部署（kill 命令最可靠）
+  player.server.runCommandSilent('kill ' + player.username)
+
   // 发送消息
   player.tell(Text.translate('msg.kubejs.suicide_note.used'))
-
-  // 使用 outOfWorld 伤害类型，高效击杀（无视护甲/抗性）
-  player.hurt('outOfWorld', 10000)
 })
