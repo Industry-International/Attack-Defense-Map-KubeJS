5253e1e46fff493fce85d231d9e65b4e54e9b09c
xkmxz <aw53978296@163.com>
2026-07-09 15:20:40 +0800

    feat: 修改 GUI 打开时的物品拾取逻辑，使用 pickedUp 事件以简化取消拾取流程

--- server_scripts/profession/profession_gui.js
+++ server_scripts/profession/profession_gui.js
@@ -902,9 +902,8 @@ PlayerEvents.loggedOut(event => {
   p.removeTag('no_loadout')
 })
 
-/** 当 GUI 打开时阻止拾取地上的物品（canPickUp 在拾取前触发，可取消） */
-ItemEvents.canPickUp(event => {
+/** 当 GUI 打开时阻止拾取地上的物品 */
+ItemEvents.pickedUp(event => {
   if (event.getEntity().persistentData.guiOpen) {
-    event.cancel()
   }
 })
